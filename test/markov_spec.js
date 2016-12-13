498e4a95cc903c9a3519d3ed3e2e6185bd9c408d
vivian <vivian@eff.org>
2016-12-12 22:01:50 -0800

    Stub _.random and _.sample

--- test/markov_spec.js
+++ test/markov_spec.js
@@ -1,58 +1,8 @@
-import { model } from '../lib/markov';
+import mockery from 'mockery';
 import assert from 'assert';
+import * as _ from 'lodash';
 
-
-describe('generating triples', () => {
-  let input = "Cold blooded jelly donut"
-
-  it('generates triples from empty input', () => {
-    assert.deepEqual(model('').triples, []);
-  });
-
-  it('from nonempty input', () => {
-    assert.deepEqual(model(input).triples, [
-      ['Cold', 'blooded', 'jelly'],
-      ['blooded', 'jelly', 'donut'],
-    ]);
-  });
-
-});
-
-
-describe('building a lookup table', () => {
-  let input = `
-    She had blue skin.
-    And so did he.
-    He kept it hid
-    And so did she.
-    They searched for blue
-    Their whole life through,
-    Then passed right by—
-    And never knew.
-  `;
-
-  it('from empty input', () => {
-    assert.deepEqual(model('').lookup, {});
-  });
-
-  it('when repeated keys are present', () => {
-    assert.deepEqual(
-      model(input).lookup[['so', 'did']],
-      ['he.\n', 'she.\n']
-    );
-  });
-
-  it('when repeated triples are present', () => {
-    assert.deepEqual(
-      model(input).lookup[['And', 'so']],
-      ['did', 'did']
-    );
-  });
-
-});
-
-describe('generating output', () => {
-  let input = `
+let input = `
  She had blue skin.
  And so did he.
  He kept it hid
@@ -63,7 +13,47 @@ describe('generating output', () => {
  And never knew.
 `;
 
-  it('generates a new string', () => {
-    console.log(model(input).generate());
+describe('markov model', () => {
+  let markov;
+
+  before(() => {
+    mockery.enable({warnOnUnregistered: false});
+    _.random = () => { return 1; }
+    _.sample = (array) => { return array[0]; }
+    mockery.registerMock("lodash", _);
+    markov = require('../lib/markov');
+  });
+
+  after(() => {
+    mockery.disable();
+  });
+
+  it('generates triples', () => {
+    assert.deepEqual(markov.model("Cold blooded jelly donut").triples, [
+      ['Cold', 'blooded', 'jelly'],
+      ['blooded', 'jelly', 'donut'],
+    ]);
+  });
+
+  it('generates a lookup table with repeated keys', () => {
+    assert.deepEqual(
+      markov.model(input).lookup[['so', 'did']],
+      ['he.\n', 'she.\n']
+    );
+  });
+
+  it('generates a lookup table with repeated triples', () => {
+    assert.deepEqual(
+      markov.model(input).lookup[['And', 'so']],
+      ['did', 'did']
+    );
+  });
+
+  it('survives empty input', () => {
+    assert.deepEqual(markov.model('').lookup, {});
+  });
+
+  it('generates novel output', () => {
+    console.log(markov.model(input).generate());
   });
 });
