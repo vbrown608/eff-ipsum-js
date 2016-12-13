71f464a2ecde27fcca564a7e9331e82e6f77c75a
vivian <vivian@eff.org>
2016-12-12 22:33:51 -0800

    Export default from markov

--- test/markov_spec.js
+++ test/markov_spec.js
@@ -5,14 +5,14 @@ import * as _ from 'lodash';
 let input = 'This is a test - a test is what this is. What is this? A test - it is.';
 
 describe('markov model', () => {
-  let markov;
+  let model;
 
   before(() => {
     mockery.enable({warnOnUnregistered: false});
     _.random = () => { return 1; }
     _.sample = (array) => { return _.last(array); }
     mockery.registerMock("lodash", _);
-    markov = require('../lib/markov');
+    model = require('../lib/markov').default;
   });
 
   after(() => {
@@ -20,7 +20,7 @@ describe('markov model', () => {
   });
 
   it('generates triples', () => {
-    assert.deepEqual(markov.model("Cold blooded jelly donut").triples, [
+    assert.deepEqual(model("Cold blooded jelly donut").triples, [
       ['Cold', 'blooded', 'jelly'],
       ['blooded', 'jelly', 'donut'],
     ]);
@@ -28,17 +28,17 @@ describe('markov model', () => {
 
   it('generates a lookup table', () => {
     assert.deepEqual(
-      markov.model(input).lookup[['a', 'test']],
+      model(input).lookup[['a', 'test']],
       ['-', 'is']
     );
   });
 
   it('survives empty input', () => {
-    assert.deepEqual(markov.model('').lookup, {});
+    assert.deepEqual(model('').lookup, {});
   });
 
   it('generates novel output', () => {
-    assert.equal(markov.model(input).generate(),
+    assert.equal(model(input).generate(),
       'is a test is what this is. What is this? ' +
       'A test - it is a test is what this is. What is this? A test'
     );
